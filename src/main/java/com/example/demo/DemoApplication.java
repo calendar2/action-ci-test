4a9a65989525a90ea9418a4e5551028c3367e3e1
차민재 <122072805+calendar2@users.noreply.github.com>
2025-02-06 16:17:15 +0900

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// 주석 ㄱㄱ
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
